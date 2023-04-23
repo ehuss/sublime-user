2c24241512f434098a6ae6715930ee51a5a86930
Eric Huss <eric@huss.org>
2023-04-23 10:48:42 -0700

    Add command to select all misspellings.

--- eric_misc.py
+++ eric_misc.py
@@ -5,6 +5,20 @@ import sublime
 import sublime_plugin
 import webbrowser
 
+import sublime, sublime_plugin
+
+class SelectAllSpellingErrorsCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        regions = []
+        while True:
+            self.view.run_command('next_misspelling')
+            if self.view.sel()[0] not in regions:
+                regions.append(self.view.sel()[0])
+            else:
+                break
+        self.view.sel().clear()
+        self.view.sel().add_all(regions)
+
 
 class EricDupeTestCommand(sublime_plugin.TextCommand):
 
